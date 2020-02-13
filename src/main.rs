e42fcf0976984ce658d09d722958efba54a53c4d
Federico Dolce <psykopear@gmail.com>
2020-02-13 20:45:13 +0100

    Fixed name and colors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use druid::widget::{Align, Flex, TextBox};
-use druid::{theme, AppLauncher, LocalizedString, PlatformError, Widget, WindowDesc};
+use druid::{theme, AppLauncher, Color, LocalizedString, PlatformError, Widget, WindowDesc};
 
 fn ui_builder() -> impl Widget<String> {
     let input = TextBox::new();
@@ -9,13 +9,18 @@ fn ui_builder() -> impl Widget<String> {
 fn main() -> Result<(), PlatformError> {
     let main_window = WindowDesc::new(ui_builder)
         .window_size((600., 100.))
-        .title(LocalizedString::new("rlaunch").with_placeholder("Rust launcher".to_string()));
+        .title(LocalizedString::new("launcherrr").with_placeholder("launcherrr".to_string()));
 
     AppLauncher::with_window(main_window)
         .configure_env(|env, _| {
             env.set(theme::BORDERED_WIDGET_HEIGHT, 100.);
             env.set(theme::TEXT_SIZE_NORMAL, 40.);
-            env.set(theme::TEXTBOX_BORDER_RADIUS, 15.);
+            env.set(theme::TEXTBOX_BORDER_RADIUS, 2.);
+            env.set(
+                theme::WINDOW_BACKGROUND_COLOR,
+                Color::rgb8(0x28, 0x2c, 0x34),
+            );
+            env.set(theme::BACKGROUND_LIGHT, Color::rgb8(0x28, 0x2c, 0x34));
         })
         .use_simple_logger()
         .launch("".to_string())?;
